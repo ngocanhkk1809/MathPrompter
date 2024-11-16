b7de2dfe0d72f5bde5ec19112ee830974dd15cbb
ngocanhkk1809 <130637222+ngocanhkk1809@users.noreply.github.com>
2024-11-16 19:11:44 +0700

    Update math_prompts_generation.py

--- math_prompts_generation.py
+++ math_prompts_generation.py
@@ -34,7 +34,7 @@ Only the algebraic expression is required, without explanations.
 
 def algebric_expression_generation(question: str, temperature: float):
     question = question.strip()
-    SERVICE = get_llm_service('openai', 'gpt-3.5-turbo', temperature=temperature, max_tokens=200)
+    SERVICE = get_llm_service('llama', 'llama3.2-1b', temperature=temperature, max_tokens=200)
     messages = SERVICE.create_prompt(system_prompt=ALGEBRAIC_SYSTEM_PROMPT, few_shot_examples=ALGEBRAIC_FEW_SHOT_PROMPT, question=question)
     response = SERVICE.make_request(messages=messages)
     expression = response.strip()
