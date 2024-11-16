0e92dfa446c39a09afb7e97cef26b34a60472ad6
ngocanhkk1809 <130637222+ngocanhkk1809@users.noreply.github.com>
2024-11-16 19:05:38 +0700

    Update llama_api.py

--- llm_inference/llama_api.py
+++ llm_inference/llama_api.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-class LLaMAService:
+class LLaMAService(LLMInterface):
     def __init__(self, model_name, temperature, max_tokens):
         self.model_name = model_name
         self.temperature = temperature
